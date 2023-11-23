b14e324e97dfda34c882c6bf37c2cc43a611d81a
gutigutioop <gutigutioop@example.com>
2023-11-23 23:02:06 +0900

    開発環境の課題です

---
+++ src/com/example/SampleTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+    //@Nested
+    //class getColor {
+        @Test
+        @DisplayName("色のチェック")
+        void success() {
+            assertEquals("赤", Sample.getColor(1));
+            assertEquals("緑", Sample.getColor(2));
+            assertEquals("青", Sample.getColor(3));
+        }
+
+        @Test
+        @DisplayName("引数エラー")
+        void assertThrows() {
+            try {
+                Sample.getColor(99);
+                fail();
+            } catch (IllegalArgumentException e) {
+            }
+        }
+    //}
+}
